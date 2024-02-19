a60993df79f42b58809f63a22843002a9f6822a3
kisahtegar <code.kisahtegar@gmail.com>
2024-02-19 23:29:08 +0700

    Interface and Implements

---
+++ src/com/dicoding/javafundamental/oop/Hewan.java
@@ -0,0 +1,13 @@
+package com.dicoding.javafundamental.oop;
+
+/**
+ * interface sangat mirip dengan kelas, tapi tanpa atribut kelas dan memiliki metode yang dideklarasikan tanpa isi.
+ * Deklarasi metode pada sebuah interface dapat diimplementasikan oleh kelas lain.
+ */
+public interface Hewan {
+    // Konstanta di kode Java biasanya ditulis dengan huruf besar semuanya dan diberi pemisah underscore jika lebih dari
+    // 1 kata. Hal ini tertuang di java coding convention bagian Constants.
+    String respirasi = "oksigen";
+
+    void makan();
+}
---
+++ src/com/dicoding/javafundamental/oop/Kucing.java
@@ -0,0 +1,16 @@
+package com.dicoding.javafundamental.oop;
+
+/**
+ * Interface harus dapat digunakan ke kelas lain. Cara untuk mengimplementasi interface yaitu menggunakan keyword
+ * implements pada kelas yang mengimplementasikannya.
+ *
+ * Dari latihan sebelumnya kita membuat interface Hewan. Kemudian kita membuat kelas Kucing yang menerapkan interface
+ * tersebut. Maka tiap metode yang berada pada interface harus diimplementasikan. Interface terlihat sebagai suatu
+ * aturan atau kontrak bagi kelas yang mengimplementasikannya.
+ */
+public abstract class Kucing implements Hewan {
+    @Override
+    public void makan() {
+
+    }
+}
