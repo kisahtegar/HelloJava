27bc2f8e4b17e6cd09083cd46251810659d39649
kisahtegar <code.kisahtegar@gmail.com>
2024-02-19 16:51:10 +0700

    Perulangan: DoWhile, For, ForBersarang, While

---
+++ src/com/dicoding/javafundamental/perulangan/For.java
@@ -0,0 +1,22 @@
+package com.dicoding.javafundamental.perulangan;
+
+public class For {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Angka : " + i);
+        }
+    }
+}
+
+//output:
+
+//Angka : 1
+//Angka : 2
+//Angka : 3
+//Angka : 4
+//Angka : 5
+//Angka : 6
+//Angka : 7
+//Angka : 8
+//Angka : 9
+//Angka : 10
